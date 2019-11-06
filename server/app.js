314296ea96d1da2d5cf757bf395ea999a9406549
dilidili <466311467@qq.com>
2019-11-06 22:16:49 +0800

    router init

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const Koa = require("koa");
+const Router = require('koa-router');
+
+
+const app = new Koa();
+const router = new Router();
+
+router.get('/classic/latest', (ctx, next) => {
+    ctx.body = {
+        key: 'classic'
+    }
+    next();
+})
+
+app
+    .use(router.routes())
+    .use(router.allowedMethods())
+
+app.listen(3003, () => {
+    console.log('App is running at port 3003')
+})
