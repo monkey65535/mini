7ca29820f65061a267e869ddf17c9dd3f5e5919f
dilidili <466311467@qq.com>
2019-11-09 16:02:53 +0800

    获取请求的参数

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 const Koa = require("koa");
+const bodyParser = require('koa-bodyparser');
 const InitManager = require('./core/init')
 
 // 创建APP
 const app = new Koa();
+app.use(bodyParser());
 InitManager.initCore(app);
 app.listen(3003, () => {
     console.log('App is running at port 3003')
--- server/app/api/v1/classic.js
+++ server/app/api/v1/classic.js
@@ -7,4 +7,17 @@ router.get('/api/v1/classic/lastest', async (ctx, next) => {
     }
 })
 
+router.post('/api/v1/:id/lastest', async (ctx, next) => {
+    const path = ctx.params;            // 获取地址栏中传递的参数
+    const query = ctx.request.query;    // 获取地址栏中#后面的参数
+    const headers = ctx.request.header; // 获取请求的headers
+    const body = ctx.request.body;      // 获取请求的payload        
+    ctx.body = {
+        path,
+        query,
+        headers,
+        body
+    }
+})
+
 module.exports = router;
